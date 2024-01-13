654f121ae5f449d9ee92297e681941406978d78d
GitHubProUser67 <127040195+GitHubProUser67@users.noreply.github.com>
2024-01-13 22:25:11 +0100

    SVO-HTTP bugfixes.
    SVO-HTTP bugfixes.
    
    Introduce a self-test for http to debug user-space problems if any.

--- HTTPServer/HTTPServer.cs
+++ HTTPServer/HTTPServer.cs
@@ -11,7 +11,7 @@ namespace HTTPServer
     {
         #region Fields
 
-        private readonly ConcurrentBag<TcpListener> _listeners = new();
+        private readonly ConcurrentDictionary<int, TcpListener> _listeners = new();
         private CancellationTokenSource _cts = null!;
         private HttpProcessor Processor;
 
@@ -29,7 +29,46 @@ namespace HTTPServer
                 Processor.AddRoute(route);
             }
 
-            Parallel.ForEach(ports, port => { CreateHTTPPortListener(port); }) ;
+            Parallel.ForEach(ports, port => { new Thread(() => CreateHTTPPortListener(port)).Start(); }) ;
+        }
+
+        private async Task TestHttpConnectionAsync(int listenerPort) // TCP Listener user space is tricky, sometimes the port silently fails to bind, causing issues. So we test.
+        {
+            Thread.Sleep(1000); // We give the server a bit of time to initialize.
+
+            for (int retryCount = 0; retryCount < 3; retryCount++)
+            {
+                try
+                {
+                    HttpClient client = new();
+                    // Set a timeout of 5 seconds (adjust as needed)
+                    client.Timeout = TimeSpan.FromSeconds(5);
+                    HttpResponseMessage response = await client.GetAsync($"http://localhost:{listenerPort}/testconn/");
+                    response.EnsureSuccessStatusCode(); // Ensure the request was successful
+                    if (await response.Content.ReadAsStringAsync() == "Youhavereachedyourdestination")
+                        return;
+                }
+                catch (Exception ex)
+                {
+                    LoggerAccessor.LogError($"Failed to send test HTTP test request: {ex}");
+                }
+            }
+
+            if (_listeners.TryRemove(listenerPort, out TcpListener? listener))
+            {
+                try
+                {
+                    listener.Stop();
+                    LoggerAccessor.LogError($"HTTP Server on port {listenerPort} failed the test, so removed and stopped. Server will restart on it's own.");
+                    new Thread(() => CreateHTTPPortListener(listenerPort)).Start();
+                }
+                catch (Exception ex)
+                {
+                    LoggerAccessor.LogError($"Error while stopping and removing HTTP Server, thread will permently abort woth exception: {ex}");
+                }
+            }
+            else
+                LoggerAccessor.LogWarn($"No listener found for port {listenerPort}.");
         }
 
         private void CreateHTTPPortListener(int listenerPort)
@@ -41,23 +80,25 @@ namespace HTTPServer
                     TcpListener listener = new(IPAddress.Any, listenerPort);
                     listener.Start();
                     LoggerAccessor.LogInfo($"HTTP Server initiated on port: {listenerPort}...");
-                    _listeners.Add(listener);
+                    _listeners.TryAdd(listenerPort, listener);
+
+                    _ = TestHttpConnectionAsync(listenerPort);
 
                     while (!_cts.Token.IsCancellationRequested)
                     {
                         try
                         {
                             TcpClient tcpClient = await listener.AcceptTcpClientAsync(_cts.Token);
-                            Thread thread = new(() =>
-                            {
-                                Processor.HandleClient(tcpClient);
-                            });
-                            thread.Start();
+                            _ = Task.Run(() => Processor.HandleClient(tcpClient));
                             Thread.Sleep(1);
                         }
                         catch (Exception ex)
                         {
-                            LoggerAccessor.LogError($"[HTTPServer] - Client loop thrown an assertion: {ex}");
+                            if (ex.HResult != 995) LoggerAccessor.LogError($"[HTTPServer] - Client loop thrown an assertion: {ex}");
+                            listener.Stop();
+
+                            if (!listener.Server.IsBound) // Check if server is closed, then, start it again.
+                                listener.Start();
                         }
                     }
                 }
@@ -71,7 +112,7 @@ namespace HTTPServer
         public Task StopAsync(CancellationToken cancellationToken)
         {
             _cts.Cancel();
-            _listeners.ToList().ForEach(x => x.Stop());
+            _listeners.Values.ToList().ForEach(x => x.Stop());
             return Task.CompletedTask;
         }
         #endregion
