92e751db048703fdd69a3b8a2fa06d626a8e5171
GitHubProUser67 <127040195+GitHubProUser67@users.noreply.github.com>
2024-01-15 19:42:19 +0100

    Fixes SSFW IGA and user logging.
    Fixes SSFW IGA and user logging.
    
    Drop the XOR non-sense.

--- SSFWServer/Program.cs
+++ SSFWServer/Program.cs
@@ -7,7 +7,6 @@ public static class SSFWServerConfiguration
 {
     public static bool SSFWCrossSave { get; set; } = true;
     public static string SSFWMinibase { get; set; } = "[]";
-    public static string SSFWAPIKey { get; set; } = "Sghig12!s52dfca97cb:0cx1550";
     public static string SSFWLegacyKey { get; set; } = "**NoNoNoYouCantHaxThis****69";
     public static string SSFWStaticFolder { get; set; } = $"{Directory.GetCurrentDirectory()}/static/wwwssfwroot";
     public static string HTTPSCertificateFile { get; set; } = $"{Directory.GetCurrentDirectory()}/static/SSL/MultiServer.pfx";
@@ -43,7 +42,6 @@ public static class SSFWServerConfiguration
             SSFWLegacyKey = config.legacyKey;
             SSFWCrossSave = config.cross_save;
             SSFWStaticFolder = config.static_folder;
-            SSFWAPIKey = config.apikey;
             HTTPSCertificateFile = config.certificate_file;
             DiscordBotToken = config.discord_bot_token;
             DiscordChannelID = config.discord_channel_id;
