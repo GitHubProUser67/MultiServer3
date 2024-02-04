4c09d31131a66e3d3ebb3dc81eb7936e1af4a876
GitHubProUser67 <127040195+GitHubProUser67@users.noreply.github.com>
2024-02-04 16:34:46 +0100

    Remove SRVEmu and MultiSpy.
    Remove SRVEmu and MultiSpy.
    
    It is impossible to maintain booth.
    
    For EA, the plan is to continue the ME3 Private server based on blaze, but I will likely only do blaze.
    
    Also, keep the vulnerable sslv3 server from arcadia, we will need it.

--- SRVEmu/Arcadia/Internal/ConnectionLogScope.cs
+++
@@ -1,7 +0,0 @@
-namespace SRVEmu.Arcadia.Internal;
-
-public record ConnectionLogScope
-{
-    public string ConnectionId { get; init; } = Guid.NewGuid().ToString();
-    public string ClientEndpoint { get; set; } = string.Empty;
-}
