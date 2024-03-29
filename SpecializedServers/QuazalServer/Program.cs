4b76bbdeca5c307494fa4d1c830eb76bdd2fcfdd
GitHubProUser67 <127040195+GitHubProUser67@users.noreply.github.com>
2024-03-29 10:33:54 +0100

    Fixes BAR Lua decryption handling, and stupid Quazal compiling issue.
    Fixes BAR Lua decryption handling, and stupid Quazal compiling issue.

--- SpecializedServers/QuazalServer/Program.cs
+++ SpecializedServers/QuazalServer/Program.cs
@@ -31,7 +31,7 @@ public static class QuazalServerConfiguration
                         Tuple.Create(61134, "ex5LYTJ0"), // WATCHDOGSPS3 // TODO, fix checksum.
                         Tuple.Create(60001, "ridfebb9"), // RB3
                         Tuple.Create(21032, "8dtRv2oj"), // GRO
-                        Tuple.Create(30561, "os4R9pEiy") // GHOSTBUSTERSPS3
+                        Tuple.Create(30561, "os4R9pEiy"), // GHOSTBUSTERSPS3
                         Tuple.Create(61136, "pJ3Lsyc2"), // WATCHDOGSWIIU // TODO, fix checksum.
                     };
     public static List<Tuple<int, int, string>>? RendezVousServersList { get; set; } = new List<Tuple<int, int, string>>
@@ -50,7 +50,7 @@ public static class QuazalServerConfiguration
                         Tuple.Create(61129, 61130, "h0rszqTw"), // AC2PS3
                         Tuple.Create(61131, 61132, "lON6yKGp"), // SCBLACKLISTPS3
                         Tuple.Create(61133, 61134, "ex5LYTJ0"), // WATCHDOGSPS3 // TODO, fix checksum.
-                        Tuple.Create(30560, 30561, "os4R9pEiy") // GHOSTBUSTERSPS3
+                        Tuple.Create(30560, 30561, "os4R9pEiy"), // GHOSTBUSTERSPS3
                         Tuple.Create(61135, 61136, "pJ3Lsyc2"), // WATCHDOGSWIIU // TODO, fix checksum.
                     };
 
