3a574dac648dca61ebd925562d195127784ad743
JumpSuit <26616362+Jump-Suit@users.noreply.github.com>
2024-04-01 12:56:00 -0500

    My Mistake begone

--- BackendServices/AuxiliaryServices/WebUtils/PREMIUMAGENCY/Trigger.cs
+++ BackendServices/AuxiliaryServices/WebUtils/PREMIUMAGENCY/Trigger.cs
@@ -406,7 +406,6 @@ namespace WebUtils.PREMIUMAGENCY
                         return File.ReadAllText($"{workpath}/eventController/j_liargame2/confirmEventTriggerCALL.xml");
                     LoggerAccessor.LogInfo($"[PREMIUMAGENCY] - ConfirmEventTrigger sent for CALL evid PUBLIC LiarGame2 {eventId}!");
                     break;
-
                 case "331":
                     if (File.Exists($"{workpath}/eventController/Macross/EventShop/confirmEventTrigger.xml"))
                         return File.ReadAllText($"{workpath}/eventController/Macross/EventShop/confirmEventTrigger.xml");
