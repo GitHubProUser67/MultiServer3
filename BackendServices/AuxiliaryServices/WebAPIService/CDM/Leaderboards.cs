86903c84dec0a19f0c2b75a7a18424ec14b10758
GitHubProUser67 <127040195+GitHubProUser67@users.noreply.github.com>
2024-04-20 21:08:57 +0200

    Fixes compile due to missing System reference.
    Fixes compile due to missing System reference.

--- BackendServices/AuxiliaryServices/WebAPIService/CDM/Leaderboards.cs
+++ BackendServices/AuxiliaryServices/WebAPIService/CDM/Leaderboards.cs
@@ -1,9 +1,5 @@
 ﻿using CustomLogger;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace WebAPIService.CDM
 {
