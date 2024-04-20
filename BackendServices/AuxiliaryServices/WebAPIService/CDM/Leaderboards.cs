e8254b58cd390ee23bd34cf5cb8eb634456f5543
JumpSuit <26616362+Jump-Suit@users.noreply.github.com>
2024-04-20 13:39:28 -0500

    CDM Leaderboards
    This PR is quickly addresses Leaderboards for Cutthroats InfoTable, until I learn how uploading is done.

---
+++ BackendServices/AuxiliaryServices/WebAPIService/CDM/Leaderboards.cs
@@ -0,0 +1,37 @@
+﻿using CustomLogger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPIService.CDM
+{
+    public class Leaderboards
+    {
+
+        public static string? handleLeaderboards(byte[] PostData, string ContentType, string workpath, string absolutePath)
+        {
+            string pubListPath = $"{workpath}/CDM/Leaderboards/";
+
+            Directory.CreateDirectory(pubListPath);
+            string filePath = $"{pubListPath}/TestLeaderboard.xml";
+            if (File.Exists(filePath))
+            {
+                LoggerAccessor.LogInfo($"[CDM] - Leaderboard found and sent! (TEMP IMPLEMENTATION)!");
+                string res = File.ReadAllText(filePath);
+
+                string resourceXML = $"{res}";
+
+                return resourceXML;
+            }
+            else
+            {
+                LoggerAccessor.LogError($"[CDM] - Failed to find Leaderboard with expected path {filePath}! (TEMP  IMPLEMENTATION)");
+
+                return "<xml><Leaderboard NAME=\"Player 1\" COINS=\"99999\" /></xml>";
+            }
+        }
+
+    }
+}
