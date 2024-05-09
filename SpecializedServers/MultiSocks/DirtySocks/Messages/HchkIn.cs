2f4524f7e9b92050902fbefd6d084d8b13c70ae1
GitHubProUser67 <127040195+GitHubProUser67@users.noreply.github.com>
2024-05-09 22:14:59 +0200

    Fixes Burnout Paradise 2nd session disconnection issue.
    Fixes Burnout Paradise 2nd session disconnection issue.
    
    Due to HCHK check failing.
    
    Thank you a LOT eaEmu.

--- SpecializedServers/MultiSocks/DirtySocks/Messages/HchkIn.cs
+++ SpecializedServers/MultiSocks/DirtySocks/Messages/HchkIn.cs
@@ -1,4 +1,6 @@
-﻿namespace MultiSocks.DirtySocks.Messages
+﻿using MultiSocks.DirtySocks.Model;
+
+namespace MultiSocks.DirtySocks.Messages
 {
     public class HchkIn : AbstractMessage
     {
@@ -6,6 +8,11 @@
 
         public override void Process(AbstractDirtySockServer context, DirtySockClient client)
         {
+            User? user = client.User;
+            if (user == null) return;
+
+            user.SendPlusWho(user, !string.IsNullOrEmpty(context.Project) && context.Project.Contains("BURNOUT5") ? "BURNOUT5" : string.Empty); // THANK YOU EAEMU https://github.com/teknogods/eaEmu/blob/master/eaEmu/ea/games/pcburnout08.py
+
             client.SendMessage(new HchkOut());
         }
     }
