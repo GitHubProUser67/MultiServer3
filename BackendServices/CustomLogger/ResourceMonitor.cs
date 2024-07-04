bf7ce71e70e17148653450a105de9af0c46c3a35
GitHubProUser67 <127040195+GitHubProUser67@users.noreply.github.com>
2024-07-04 22:14:59 +0200

    LoggerAccessor.cs - Removed initiated check for each individual logger funcs.
    LoggerAccessor.cs - Removed initiated check for each individual logger funcs.
    
    C# will assert is something is wrong anyway, and so far, we don't butcher our code so... :)

--- BackendServices/CustomLogger/ResourceMonitor.cs
+++ BackendServices/CustomLogger/ResourceMonitor.cs
@@ -11,7 +11,7 @@ namespace CustomLogger
         {
             long idleTime, kernelTime, userTime;
 
-            while (LoggerAccessor.initiated)
+            while (true)
             {
                 // Sleep for 1 minute (60,000 milliseconds)
                 Thread.Sleep(60000);
@@ -21,8 +21,6 @@ namespace CustomLogger
                 if (PerformanceInfo.GetSystemTimes(out idleTime, out kernelTime, out userTime))
                     LoggerAccessor.LogInfo($"[ResourceMonitor] - Current CPU Load: {(100.0 - ((idleTime / (double)(kernelTime + userTime)) * 100.0)):0.##}%");
             }
-
-            return Task.CompletedTask;
         }
     }
 
