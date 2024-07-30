07412cdad0adaffdfdda53967bccddac55541c5e
GitHubProUser67 <127040195+GitHubProUser67@users.noreply.github.com>
2024-07-30 19:07:51 +0200

    Port Pull Request https://github.com/GitHubProUser67/MultiServer3/pull/68
    Port Pull Request https://github.com/GitHubProUser67/MultiServer3/pull/68

---
+++ BackendServices/AuxiliaryServices/WebAPIService/I_Love_Sony/ILoveSonyClass.cs
@@ -0,0 +1,76 @@
+using CyberBackendLibrary.HTTP;
+using System;
+
+namespace WebAPIService.ILoveSony
+{
+    public class ILoveSonyClass : IDisposable
+    {
+        private string workpath;
+        private string absolutepath;
+        private string method;
+        private bool disposedValue;
+
+        public ILoveSonyClass(string method, string absolutepath, string workpath)
+        {
+            this.absolutepath = absolutepath;
+            this.workpath = workpath;
+            this.method = method;
+        }
+
+        public string ProcessRequest(byte[] PostData, string ContentType, bool https)
+        {
+            if (string.IsNullOrEmpty(absolutepath))
+                return null;
+
+            switch (method)
+            {
+                case "GET":
+                    switch (absolutepath)
+                    {
+                        #region Resistance Fall of Man EULA
+                        case "/i_love_sony/legal/UP9000-BCUS98107_00/1":
+                            return MyResistanceEula.ILoveSonyEula();
+                        #endregion
+
+                        default:    
+                            break;
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            return null;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    absolutepath = string.Empty;
+                    method = string.Empty;
+                }
+
+                // TODO: libérer les ressources non managées (objets non managés) et substituer le finaliseur
+                // TODO: affecter aux grands champs une valeur null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: substituer le finaliseur uniquement si 'Dispose(bool disposing)' a du code pour libérer les ressources non managées
+        // ~HELLFIREClass()
+        // {
+        //     // Ne changez pas ce code. Placez le code de nettoyage dans la méthode 'Dispose(bool disposing)'
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Ne changez pas ce code. Placez le code de nettoyage dans la méthode 'Dispose(bool disposing)'
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ BackendServices/AuxiliaryServices/WebAPIService/I_Love_Sony/MyResistanceEula.cs
@@ -0,0 +1,16 @@
+namespace WebAPIService.ILoveSony
+{
+    public class MyResistanceEula
+    {
+        public static string ILoveSonyEula()
+        {
+                return $@"TERMS OF SERVICE
+                    Download is subject to the PLAYSTATION®Network Terms of Service/User Agreement and any specific additional conditions applying to this item. If you do not wish to accept these terms, do not  download this item. One time fee for use of downloads on up to 5 activated PS3™ systems. Resistance: Fall of Man © 2008 Sony Computer Entertainment America Inc.  Published by Sony Computer Entertainment America.  Developed by Insomniac Entertainment.  Resistance: Fall of Man is a trademark of Sony Computer Entertainment America Inc.  All rights reserved.
+                    2-4 Players
+                    Network Players 2-40
+                    486KB minimum save size
+                    HDTV screen resolution: 720p, 1080i";
+            
+        }
+    }
+}
