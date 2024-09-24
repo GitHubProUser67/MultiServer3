24d7aae1b6f5937d48b3c9393b23a172a71a0a76
GitHubProUser67 <127040195+GitHubProUser67@users.noreply.github.com>
2024-09-24 18:26:53 +0200

    [CyberBackendLibrary] - Rework ConcurrentList.
    [CyberBackendLibrary] - Rework ConcurrentList.
    
    Does a much better job than the "largely horrible" previous implementation.

--- BackendServices/CyberBackendLibrary/Extension/ConcurrentList.cs
+++ BackendServices/CyberBackendLibrary/Extension/ConcurrentList.cs
@@ -1,442 +1,439 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading;
 
 namespace CyberBackendLibrary.Extension
 {
-    public class ConcurrentList<T> : IEnumerable, IEnumerable<T>
-    {
-        private readonly List<T> _list = new List<T>();
-        private readonly object _lock = new object();
+    // https://gist.github.com/ronnieoverby/11c8b6b067872db719d7
+    public class ConcurrentList<T> : IList<T>, IDisposable
+	{
+		private ReaderWriterLockSlim _lock = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
+		private int _size = 0;
+		
+		public int Count
+		{
+			get
+			{ 
+				_lock.EnterReadLock();
+				try
+				{	        
+					return _size;
+				}
+				finally
+				{
+					_lock.ExitReadLock();
+				}
+			}
+		}
+		
+		public int InternalArrayLength
+		{ 
+			get
+			{ 
+				_lock.EnterReadLock();
+				try
+				{	        
+					return _items.Length;
+				}
+				finally
+				{
+					_lock.ExitReadLock();
+				}
+			}
+		}
+		
+		private T[] _items;
+		
+		public ConcurrentList(int initialCapacity)
+		{
+			_items = new T[initialCapacity];
+		}
+		
+		public ConcurrentList():this(4)
+		{ }
+		
+		public ConcurrentList(IEnumerable<T> items)
+		{
+			_items = items.ToArray();
+			_size = _items.Length;
+		}
+		
+		public void Add(T item)
+		{
+			_lock.EnterWriteLock();
+			try
+			{	  	
+				int newCount = _size + 1;			
+				EnsureCapacity(newCount);			
+				_items[_size] = item;
+				_size = newCount;      			
+			}
+			finally
+			{
+				_lock.ExitWriteLock();
+			}		
+		}
+		
+		public void AddRange(IEnumerable<T> items)
+		{
+			if (items == null)
+				throw new ArgumentNullException("items");
+				
+			_lock.EnterWriteLock();
+			try
+			{
+				T[] arr = items as T[] ?? items.ToArray();			
+				int newCount = _size + arr.Length;
+				EnsureCapacity(newCount);			
+				Array.Copy(arr, 0, _items, _size, arr.Length);		
+				_size = newCount;
+			}
+			finally
+			{
+				_lock.ExitWriteLock();			
+			}
+		}
+		
+		private void EnsureCapacity(int capacity)
+		{	
+			if (_items.Length >= capacity)
+				return;
+				
+			int doubled;
+			checked
+			{
+				try
+				{	        
+					doubled = _items.Length * 2;
+				}
+				catch (OverflowException)
+				{
+					doubled = int.MaxValue;
+				}
+			}
+		
+			int newLength = Math.Max(doubled, capacity);			
+			Array.Resize(ref _items, newLength);
+		}
+		
+		public bool Remove(T item)
+		{
+			_lock.EnterUpgradeableReadLock();
+			try
+			{	        
+				int i = IndexOfInternal(item);
+				
+				if (i == -1)
+					return false;
+					
+				_lock.EnterWriteLock();
+				try
+				{	
+					RemoveAtInternal(i);
+					return true;
+				}
+				finally
+				{				
+					_lock.ExitWriteLock();
+				}
+			}
+			finally
+			{			
+				_lock.ExitUpgradeableReadLock();
+			}
+		}
+		
+		public IEnumerator<T> GetEnumerator()
+		{
+			_lock.EnterReadLock();
+			try
+			{	 
+				for (int i = 0; i < _size; i++)
+					// deadlocking potential mitigated by lock recursion enforcement
+					yield return _items[i]; 
+			}
+			finally
+			{			
+				_lock.ExitReadLock();
+			}
+		}
+		
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+		
+		public int IndexOf(T item)
+		{
+			_lock.EnterReadLock();
+			try
+			{	
+				return IndexOfInternal(item);
+			}
+			finally
+			{
+				_lock.ExitReadLock();
+			}
+		}
+		
+		private int IndexOfInternal(T item)
+		{
+			return Array.FindIndex(_items, 0, _size, x => x.Equals(item));
+		}
+		
+		public void Insert(int index, T item)
+		{
+			_lock.EnterUpgradeableReadLock();
+			try
+			{	        			
+				if (index > _size)
+					throw new ArgumentOutOfRangeException("index");	
+			
+				_lock.EnterWriteLock();
+				try
+				{		
+					int newCount = _size + 1;
+					EnsureCapacity(newCount);
+					
+					// shift everything right by one, starting at index
+					Array.Copy(_items, index, _items, index + 1, _size - index);
+					
+					// insert
+					_items[index] = item;		
+					_size = newCount;
+				}
+				finally
+				{			
+					_lock.ExitWriteLock();
+				}
+			}
+			finally
+			{
+				_lock.ExitUpgradeableReadLock();			
+			}
+		
 
-        public ConcurrentList() { }
-
-        public ConcurrentList(int capacity)
-        {
-            lock (_lock)
-                _list = new List<T>(capacity);
-        }
-
-        public ConcurrentList(T item)
-        {
-            lock (_lock)
-                _list.Add(item);
-        }
-
-        public ConcurrentList(List<T> collection)
-        {
-            lock (_lock)
-                _list.AddRange(collection);
-        }
-
-        public ConcurrentList(IList<T> collection)
-        {
-            lock (_lock)
-                _list.AddRange(collection);
-        }
-
-        public ConcurrentList(IEnumerable<T> collection)
-        {
-            lock (_lock)
-                _list.AddRange(collection);
-        }
-
-        public ConcurrentList(ICollection<T> collection)
-        {
-            lock (_lock)
-                _list.AddRange(collection);
-        }
-
-        public ConcurrentList(IReadOnlyList<T> collection)
-        {
-            lock (_lock)
-                _list.AddRange(collection);
-        }
-
-        public ConcurrentList(IReadOnlyCollection<T> collection)
-        {
-            lock (_lock)
-                _list.AddRange(collection);
-        }
-
-        public ConcurrentList(params T[] items)
-        {
-            lock (_lock)
-                _list.AddRange(items);
-        }
-
-        public ReadOnlyCollection<T> AsReadOnly()
-        {
-            lock (_lock)
-                return _list.AsReadOnly();
-        }
-
-        public int BinarySearch(int index, int count, T item, IComparer<T> comparer)
-        {
-            lock (_lock)
-                return _list.BinarySearch(index, count, item, comparer);
-        }
-
-        public int BinarySearch(T item)
-        {
-            lock (_lock)
-                return _list.BinarySearch(item);
-        }
-
-        public int BinarySearch(T item, IComparer<T> comparer)
-        {
-            lock (_lock)
-                return _list.BinarySearch(item, comparer);
-        }
-
-        public List<TOutput> ConvertAll<TOutput>(Converter<T, TOutput> converter)
-        {
-            lock (_lock)
-                return _list.ConvertAll(converter);
-        }
-
-        public void CopyTo(T[] array, int arrayIndex)
-        {
-            lock (_lock)
-                _list.CopyTo(array, arrayIndex);
-        }
-
-        public void CopyTo(T[] array)
-        {
-            lock (_lock)
-                _list.CopyTo(array);
-        }
-
-        public void CopyTo(int index, T[] array, int arrayIndex, int count)
-        {
-            lock (_lock)
-                _list.CopyTo(index, array, arrayIndex, count);
-        }
-
-        public int EnsureCapacity(int capacity)
-        {
-            lock (_lock)
-                return _list.EnsureCapacity(capacity);
-        }
-
-        public bool Exists(Predicate<T> match)
-        {
-            lock (_lock)
-                return _list.Exists(match);
-        }
-
-        public T Find(Predicate<T> match)
-        {
-            lock (_lock)
-                return _list.Find(match);
-        }
-
-        public List<T> FindAll(Predicate<T> match)
-        {
-            lock (_lock)
-                return _list.FindAll(match);
-        }
-
-        public int FindIndex(int startIndex, int count, Predicate<T> match)
-        {
-            lock (_lock)
-                return _list.FindIndex(startIndex, count, match);
-        }
-
-        public int FindIndex(int startIndex, Predicate<T> match)
-        {
-            lock (_lock)
-                return _list.FindIndex(startIndex, match);
-        }
-
-        public int FindIndex(Predicate<T> match)
-        {
-            lock (_lock)
-                return _list.FindIndex(match);
-        }
-
-        public T FindLast(Predicate<T> match)
-        {
-            lock (_lock)
-                return _list.FindLast(match);
-        }
-
-        public int FindLastIndex(int startIndex, int count, Predicate<T> match)
-        {
-            lock (_lock)
-                return _list.FindLastIndex(startIndex, count, match);
-        }
-
-        public int FindLastIndex(int startIndex, Predicate<T> match)
-        {
-            lock (_lock)
-                return _list.FindLastIndex(startIndex, match);
-        }
-
-        public int FindLastIndex(Predicate<T> match)
-        {
-            lock (_lock)
-                return _list.FindLastIndex(match);
-        }
-
-        public void ForEach(Action<T> action)
-        {
-            lock (_lock)
-                _list.ForEach(action);
-        }
-
-        public List<T> GetRange(int index, int count)
-        {
-            lock (_lock)
-                return _list.GetRange(index, count);
-        }
-
-        public void InsertRange(int index, IEnumerable<T> collection)
-        {
-            lock (_lock)
-                _list.InsertRange(index, collection);
-        }
-
-        public void Add(T item)
-        {
-            lock (_lock)
-                _list.Add(item);
-        }
-
-        public void AddRange(List<T> item)
-        {
-            lock (_lock)
-                _list.AddRange(item);
-        }
-
-        public bool Contains(T checkitem)
-        {
-            lock (_lock)
-                return _list.Contains(checkitem);
-        }
-
-        public T Get(int index)
-        {
-            lock (_lock)
-                return _list[index];
-        }
-
-        public T Min()
-        {
-            lock (_lock)
-                return _list.Min();
-        }
-
-        public T Max()
-        {
-            lock (_lock)
-                return _list.Max();
-        }
-
-        public bool Remove(T item)
-        {
-            lock (_lock)
-                return _list.Remove(item);
-        }
-
-        public void RemoveAt(int index)
-        {
-            lock (_lock)
-                _list.RemoveAt(index);
-        }
+		}
 
         public int RemoveAll(Predicate<T> match)
         {
-            lock (_lock)
-                return _list.RemoveAll(match);
-        }
+            if (match == null)
+                throw new ArgumentNullException("match");
 
-        public void RemoveRange(int index, int count)
-        {
-            lock (_lock)
-                _list.RemoveRange(index, count);
-        }
-
-        public void Reverse(int index, int count)
-        {
-            lock (_lock)
-                _list.Reverse(index, count);
-        }
-
-        public void Reverse()
-        {
-            lock (_lock)
-                _list.Reverse();
-        }
-
-#if NET8_0_OR_GREATER
-        public List<T> Slice(int start, int length)
-        {
-            lock (_lock)
-                return _list.Slice(start, length);
-        }
-#endif
-
-        public void Sort(IComparer<T> comparer)
-        {
-            lock (_lock)
-                _list.Sort(comparer);
-        }
-
-        public void Sort(Comparison<T> comparison)
-        {
-            lock (_lock)
-                _list.Sort(comparison);
-        }
-
-        public void Sort(int index, int count, IComparer<T> comparer)
-        {
-            lock (_lock)
-                _list.Sort(index, count, comparer);
-        }
-
-        public void Sort()
-        {
-            lock (_lock)
-                _list.Sort();
-        }
-
-        public void TrimExcess()
-        {
-            lock (_lock)
-                _list.TrimExcess();
-        }
-
-        public bool TrueForAll(Predicate<T> match)
-        {
-            lock (_lock)
-                return _list.TrueForAll(match);
-        }
-
-        public void Clear()
-        {
-            lock (_lock)
-                _list.Clear();
-        }
-
-        public T FirstOrDefault(Func<T, bool> predicate)
-        {
-            lock (_lock)
-                return _list.FirstOrDefault(predicate);
-        }
-
-        public bool Any(Func<T, bool> predicate)
-        {
-            lock (_lock)
-                return _list.Any(predicate);
-        }
-
-        public bool Any()
-        {
-            lock (_lock)
-                return _list.Count > 0;
-        }
-
-        public int IndexOf(T item)
-        {
-            lock (_lock)
-                return _list.IndexOf(item);
-        }
-
-        public int IndexOf(T item, int index)
-        {
-            lock (_lock)
-                return _list.IndexOf(item, index);
-        }
-
-        public int IndexOf(T item, int index, int count)
-        {
-            lock (_lock)
-                return _list.IndexOf(item, index, count);
-        }
-
-        public int LastIndexOf(T item)
-        {
-            lock (_lock)
-                return _list.LastIndexOf(item);
-        }
-
-        public int LastIndexOf(T item, int index)
-        {
-            lock (_lock)
-                return _list.LastIndexOf(item, index);
-        }
-
-        public int LastIndexOf(T item, int index, int count)
-        {
-            lock (_lock)
-                return _list.LastIndexOf(item, index, count);
-        }
-
-        public void Insert(int index, T item)
-        {
-            lock (_lock)
-                _list.Insert(index, item);
-        }
-
-        public List<T> ToList()
-        {
-            List<T> newList = new List<T>();
-
-            lock (_lock)
-                _list.ForEach(newList.Add);
-
-            return newList;
-        }
-
-        public IEnumerator GetEnumerator()
-        {
-            return ToList().GetEnumerator();
-        }
-
-        IEnumerator<T> IEnumerable<T>.GetEnumerator()
-        {
-            return ToList().GetEnumerator();
-        }
-
-        public bool IsReadOnly => false;
-
-        public bool IsSynchronized => true;
-
-        public object SyncRoot => _lock;
-
-        public int Count
-        {
-            get
+            _lock.EnterUpgradeableReadLock();
+            try
             {
-                lock (_lock)
-                    return _list.Count;
+                int freeIndex = 0;   // the first free slot in items array
+
+                // Find the first item which needs to be removed.
+                while (freeIndex < _size && !match(_items[freeIndex])) freeIndex++;
+                if (freeIndex >= _size) return 0;
+
+                _lock.EnterWriteLock();
+                try
+                {
+                    int current = freeIndex + 1;
+                    while (current < _size)
+                    {
+                        // Find the first item which needs to be kept.
+                        while (current < _size && match(_items[current])) current++;
+
+                        if (current < _size)
+                        {
+                            // copy item to the free slot.
+                            _items[freeIndex++] = _items[current++];
+                        }
+                    }
+
+                    Array.Clear(_items, freeIndex, _size - freeIndex);
+                    int result = _size - freeIndex;
+                    _size = freeIndex;
+                    return result;
+                }
+                finally
+                {
+                    _lock.ExitWriteLock();
+                }
+            }
+            finally
+            {
+                _lock.ExitUpgradeableReadLock();
             }
         }
 
-        public int Capacity
+        public void RemoveAt(int index)
+		{	
+			_lock.EnterUpgradeableReadLock();
+			try
+			{	
+				if (index >= _size)
+					throw new ArgumentOutOfRangeException("index");
+			
+				_lock.EnterWriteLock();
+				try
+				{	        
+					RemoveAtInternal(index);
+				}
+				finally
+				{
+					_lock.ExitWriteLock();
+				}
+			}
+			finally
+			{
+				_lock.ExitUpgradeableReadLock();			
+			}
+		}
+		
+		private void RemoveAtInternal(int index)
+		{			
+			Array.Copy(_items, index + 1, _items, index, _size - index-1);
+			_size--;
+			
+			// release last element
+			Array.Clear(_items, _size, 1);
+		}
+		
+		public void Clear()
+		{
+			_lock.EnterWriteLock();
+			try
+			{        
+				Array.Clear(_items, 0, _size);
+				_size = 0;
+			}
+			finally
+			{			
+				_lock.ExitWriteLock();
+			}	
+		}
+
+        public T Find(Predicate<T> match)
         {
-            get
+            if (match == null)
+                throw new ArgumentNullException("match");
+
+            _lock.EnterUpgradeableReadLock();
+            try
             {
-                lock (_lock)
-                    return _list.Capacity;
+                for (int i = 0; i < _size; i++)
+                {
+                    if (match(_items[i]))
+                        return _items[i];
+                }
+                return default(T);
+            }
+            finally
+            {
+                _lock.ExitUpgradeableReadLock();
             }
         }
 
-        public T this[int index]
-        {
-            get
-            {
-                lock (_lock)
-                    return _list[index];
-            }
-            set
-            {
-                lock (_lock)
-                    _list[index] = value;
-            }
-        }
-    }
+        public bool Contains(T item)
+		{
+			_lock.EnterReadLock();
+			try
+			{	
+				return IndexOfInternal(item) != -1;
+			}
+			finally
+			{			
+				_lock.ExitReadLock();
+			}
+		}
+		
+		public void CopyTo(T[] array, int arrayIndex)
+		{		
+			_lock.EnterReadLock();
+			try
+			{	        
+				if(_size > array.Length - arrayIndex)
+					throw new ArgumentException("Destination array was not long enough.");
+				
+				Array.Copy(_items, 0, array, arrayIndex, _size);
+			}
+			finally
+			{
+				_lock.ExitReadLock();			
+			}
+		}
+		
+		public bool IsReadOnly
+		{	
+			get { return false; }
+		}
+		
+		public T this[int index]
+		{
+			get
+			{
+				_lock.EnterReadLock();
+				try
+				{	        
+					if (index >= _size)
+						throw new ArgumentOutOfRangeException("index");
+					
+					return _items[index];	
+				}
+				finally
+				{
+					_lock.ExitReadLock();				
+				}			
+			}
+			set
+			{
+				_lock.EnterUpgradeableReadLock();
+				try
+				{
+					
+					if (index >= _size)
+						throw new ArgumentOutOfRangeException("index");
+		
+					_lock.EnterWriteLock();
+					try
+					{						
+						_items[index] = value;
+					}
+					finally
+					{
+						_lock.ExitWriteLock();				
+					}
+				}
+				finally
+				{
+					_lock.ExitUpgradeableReadLock();
+				}
+				
+			}
+		}
+		
+		public void DoSync(Action<ConcurrentList<T>> action)
+		{
+			GetSync(l =>
+			{
+				action(l);
+				return 0;
+			});
+		}
+		
+		public TResult GetSync<TResult>(Func<ConcurrentList<T>,TResult> func)
+		{
+			_lock.EnterWriteLock();
+			try
+			{	        
+				return func(this);
+			}
+			finally
+			{
+				_lock.ExitWriteLock();
+			}
+		}
+		
+		public void Dispose()
+		{	
+			_lock.Dispose();
+		}
+	}
 }
