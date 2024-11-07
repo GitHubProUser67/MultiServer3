7da0cafe3d7b4a7683f5c70427250e1737415273
JumpSuit <26616362+Jump-Suit@users.noreply.github.com>
2024-11-07 06:26:09 -0600

    HTTPProcessor API update
    Update Dust514 crest API
    Add Ubisoft's Build DB API

---
+++ BackendServices/AuxiliaryServices/WebAPIService/UBISOFT/BuildAPI/BuildDBPullService/BuildDBPullServiceHandler.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace WebAPIService.UBISOFT.BuildAPI.BuildDBPullService
+{
+    public class BuildDBPullServiceHandler
+    {
+
+        public static string buildDBRequestParser(byte[] PostData, string ContentType)
+        {
+
+            // Load the XML string into an XDocument
+            XDocument xdoc = XDocument.Parse(Encoding.UTF8.GetString(PostData));
+
+            // Define the namespaces used in the XML document
+            XNamespace soap = "http://www.w3.org/2003/05/soap-envelope";
+            XNamespace responseNamespace = "http://builddatabasepullapi/";
+
+            var resultElement = xdoc.Descendants(soap + "Body").FirstOrDefault();
+            string innerRequestName = resultElement.Name.ToString();
+
+            List<string> buildDbRequests = new List<string>() 
+            { "GetCurrentLauncherVersion", "GetConsoleOwner",
+                "GetProjectDetailWithMacValidation", "GetFilteredBuildVersionsWithMacValidation",
+                "GetProjectsWithMacValidation", "GetFilteredBuildVersionsWithMacValidation"};
+
+
+            if (buildDbRequests.Contains(innerRequestName))
+            {
+                switch (innerRequestName) {
+                    case "GetCurrentLauncherVersion":
+                        {
+
+                            Console.WriteLine($"GetCurrentLauncherVersion: TRIGGERED!");
+
+                            return @"<?xml version=""1.0"" encoding=""utf-8""?>
+<soap:Envelope
+  xmlns:soap=""http://www.w3.org/2003/05/soap-envelope""
+  xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance""
+  xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
+  <soap:Body>
+    <LauncherVersion
+      xmlns=""http://builddatabasepullapi/"">
+        <VersionName>2.10.1</VersionName>
+        <VersionPath>/BuildDBPullService.asmx</VersionPath>
+        <LauncherVersionId>4</LauncherVersionId>
+        <PlatformId>4</PlatformId>
+    </LauncherVersion>
+  </soap:Body>
+</soap:Envelope>";
+                        }
+
+                    default:
+                        {
+
+                            Console.WriteLine($"GetCurrentLauncherVersion: FAILEd!");
+                            return @"<?xml version=""1.0"" encoding=""utf-8""?>
+<soap:Envelope
+  xmlns:soap=""http://www.w3.org/2003/05/soap-envelope""
+  xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance""
+  xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
+  <soap:Body>
+    <LauncherVersion
+      xmlns=""http://builddatabasepullapi/"">
+        <VersionName>2.10.1</VersionName>
+        <VersionPath>/BuildDBPullService.asmx</VersionPath>
+        <LauncherVersionId>4</LauncherVersionId>
+        <PlatformId>4</PlatformId>
+    </LauncherVersion>
+  </soap:Body>
+</soap:Envelope>";
+                        }
+
+                }
+
+
+                //Console.WriteLine($"ADLogin: {adLogin}");
+                //Console.WriteLine($"Name: {name}");
+            }
+            else
+            {
+                Console.WriteLine("No GetConsoleOwnerResult found.");
+            }
+
+            /*
+            // Get the GetConsoleOwner element from the SOAP body
+            var resultElement = xdoc.Descendants(soap + "Body").FirstOrDefault();
+
+            if (buildDbRequests.Contains(resultElement.Name.ToString()))
+            {
+                // Extract the ADLogin and Name elements
+                string adLogin = resultElement.Element("ADLogin")?.Value;
+                string name = resultElement.Element("Name")?.Value;
+
+                Console.WriteLine($"ADLogin: {adLogin}");
+                Console.WriteLine($"Name: {name}");
+            }
+            else
+            {
+                Console.WriteLine("No GetConsoleOwnerResult found.");
+            }
+
+            */
+
+
+            return @"<?xml version=""1.0"" encoding=""utf-8""?>
+<soap:Envelope
+  xmlns:soap=""http://www.w3.org/2003/05/soap-envelope""
+  xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance""
+  xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
+  <soap:Body>
+    <LauncherVersion
+      xmlns=""http://builddatabasepullapi/"">
+        <VersionName>2.10.2</VersionName>
+        <VersionPath>/BuildDBPullService.asmx</VersionPath>
+        <LauncherVersionId>4</LauncherVersionId>
+        <PlatformId>4</PlatformId>
+    </LauncherVersion>
+  </soap:Body>
+</soap:Envelope>";
+
+
+        }
+
+
+        public static string getVersion(byte[] PostData, string ContentType) {
+
+
+            return @"<?xml version=""1.0"" encoding=""utf-8""?>
+<soap:Envelope
+  xmlns:soap=""http://www.w3.org/2003/05/soap-envelope""
+  xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance""
+  xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
+  <soap:Body>
+    <GetConsoleOwnerResponse
+      xmlns=""http://builddatabasepullapi/"">
+      <GetConsoleOwnerResult>
+        <ADLogin>hensley.edwin</ADLogin>
+        <Name>Hensley Edwin</Name>
+      </GetConsoleOwnerResult>
+    </GetConsoleOwnerResponse>
+  </soap:Body>
+</soap:Envelope>";
+        }
+
+        public static string getConsoleOwner(byte[] PostData, string ContentType)
+        {
+
+
+            return @"<?xml version=""1.0"" encoding=""utf-8""?>
+<soap:Envelope
+  xmlns:soap=""http://www.w3.org/2003/05/soap-envelope""
+  xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance""
+  xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
+  <soap:Body>
+    <GetConsoleOwnerResponse
+      xmlns=""http://builddatabasepullapi/"">
+      <GetConsoleOwnerResult>
+        <ADLogin>hensley.edwin</ADLogin>
+        <Name>Hensley Edwin</Name>
+      </GetConsoleOwnerResult>
+    </GetConsoleOwnerResponse>
+  </soap:Body>
+</soap:Envelope>";
+        }
+    }
+}
---
+++ BackendServices/AuxiliaryServices/WebAPIService/UBISOFT/BuildAPI/SoapBuildAPIClass.cs
@@ -0,0 +1,89 @@
+﻿using CustomLogger;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPIService.OUWF;
+using WebAPIService.UBISOFT.BuildAPI.BuildDBPullService;
+
+namespace WebAPIService.UBISOFT.BuildAPI
+{
+
+    public class SoapBuildAPIClass : IDisposable
+    {
+        string workpath;
+        string absolutepath;
+        string method;
+        private bool disposedValue;
+
+        public SoapBuildAPIClass(string method, string absolutepath, string workpath)
+        {
+            this.workpath = workpath;
+            this.absolutepath = absolutepath;
+            this.method = method;
+        }
+
+        public string ProcessRequest(byte[] PostData, string ContentType)
+        {
+            if (string.IsNullOrEmpty(absolutepath))
+                return null;
+
+            switch (method)
+            {
+                case "POST":
+                    switch (absolutepath)
+                    {
+
+                        case "/BuildDBPullService.asmx":
+                            return BuildDBPullServiceHandler.buildDBRequestParser(PostData, ContentType);
+                        default:
+                            {
+                                LoggerAccessor.LogError($"[OuWF] - Unhandled server request discovered: {absolutepath} | DETAILS: \n{Encoding.UTF8.GetString(PostData)}");
+                            }
+                            break;
+                    }
+                    break;
+                default:
+                    {
+                        LoggerAccessor.LogError($"[OuWF] - Method unhandled {method}");
+                    }
+                    break;
+            }
+
+            return null;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    absolutepath = string.Empty;
+                    method = string.Empty;
+                    workpath = string.Empty;
+                }
+
+                // TODO: libérer les ressources non managées (objets non managés) et substituer le finaliseur
+                // TODO: affecter aux grands champs une valeur null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: substituer le finaliseur uniquement si 'Dispose(bool disposing)' a du code pour libérer les ressources non managées
+        // ~HERMESClass()
+        // {
+        //     // Ne changez pas ce code. Placez le code de nettoyage dans la méthode 'Dispose(bool disposing)'
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Ne changez pas ce code. Placez le code de nettoyage dans la méthode 'Dispose(bool disposing)'
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
