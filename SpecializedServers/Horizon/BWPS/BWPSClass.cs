c2d22c38a67d63eb1259ab7902cf958495ebeca4
GitHubProUser67 <127040195+GitHubProUser67@users.noreply.github.com>
2025-01-25 13:36:50 +0100

    [Horizon] - Implements partial BWPS support.
    This seems enough for the games that needs it, it will simply connect, and not measure anything for now.

--- SpecializedServers/Horizon/BWPS/BWPSClass.cs
+++ SpecializedServers/Horizon/BWPS/BWPSClass.cs
@@ -2,8 +2,6 @@ using CustomLogger;
 using Newtonsoft.Json;
 using Horizon.BWPS.Config;
 using Horizon.LIBRARY.Common;
-using System.Diagnostics;
-using Horizon.PluginManager;
 
 namespace Horizon.BWPS
 {
@@ -11,82 +9,12 @@ namespace Horizon.BWPS
     {
         public static string? CONFIG_FILE = HorizonServerConfiguration.BWPSConfig;
 
+        public const string gpszVersion2 = "3.02.200704101920";
+
         public static ServerSettings Settings = new();
         public static BWPS BWPS = new();
 
-        public static MediusPluginsManager Plugins = new(HorizonServerConfiguration.PluginsFolder);
-
-        public static readonly Stopwatch Stopwatch = Stopwatch.StartNew();
-
-        private static DateTime _timeLastPluginTick = Utils.GetHighPrecisionUtcTime();
-
-        private static DateTime _lastConfigRefresh = Utils.GetHighPrecisionUtcTime();
-
         public static bool started = false;
-
-        private static async Task TickAsync()
-        {
-            try
-            {
-                await HandleInMessages();
-
-                // Tick plugins
-                if ((Utils.GetHighPrecisionUtcTime() - _timeLastPluginTick).TotalMilliseconds > Settings.PluginTickIntervalMs)
-                {
-                    _timeLastPluginTick = Utils.GetHighPrecisionUtcTime();
-                    await Plugins.Tick();
-                }
-
-                await HandleOutMessages();
-
-                // Reload config
-                if ((Utils.GetHighPrecisionUtcTime() - _lastConfigRefresh).TotalMilliseconds > Settings.RefreshConfigInterval)
-                {
-                    RefreshConfig();
-                    _lastConfigRefresh = Utils.GetHighPrecisionUtcTime();
-                }
-            }
-            catch (Exception ex)
-            {
-                LoggerAccessor.LogError(ex);
-            }
-        }
-
-        private static async Task HandleInMessages()
-        {
-            // handle incoming
-            {
-                var tasks = new List<Task>()
-                {
-                    BWPS.HandleIncomingMessages()
-                };
-
-                await Task.WhenAll(tasks);
-            }
-        }
-
-        private static async Task HandleOutMessages()
-        {
-            // handle outgoing
-            {
-                var tasks = new List<Task>()
-                {
-                    BWPS.HandleOutgoingMessages()
-                };
-
-                await Task.WhenAll(tasks);
-            }
-        }
-
-        private static async Task LoopServer()
-        {
-            while (started)
-            {
-                await TickAsync();
-
-                await Task.Delay(100);
-            }
-        }
 
         public static async void StopServer()
         {
@@ -106,8 +34,7 @@ namespace Horizon.BWPS
                 LoggerAccessor.LogInfo($"* Launched on {datetime}");
 
                 Task.WaitAll(BWPS.Start());
-                //string gpszVersion = "rt_bwprobe ReleaseVersion 3.02.200704101920";
-                string gpszVersion2 = "3.02.200704101920";
+
                 LoggerAccessor.LogInfo($"* Bandwidth Probe Server Version {gpszVersion2}");
 
                 //* Process ID: %d , Parent Process ID: %d
@@ -121,8 +48,6 @@ namespace Horizon.BWPS
                 LoggerAccessor.LogInfo($"UDP BWPS started on port {Settings.BWPSPort}.");
 
                 started = true;
-
-                _ = Task.Run(LoopServer);
             }
             catch (Exception ex)
             {
