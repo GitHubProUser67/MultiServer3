765fd00f4e1c0a810cf0b8f79cc45928c00231e3
GitHubProUser67 <127040195+GitHubProUser67@users.noreply.github.com>
2025-01-25 13:36:50 +0100

    [Horizon] - Implements partial BWPS support.
    This seems enough for the games that needs it, it will simply connect, and not measure anything for now.

--- SpecializedServers/Horizon/BWPS/BWPS.cs
+++ SpecializedServers/Horizon/BWPS/BWPS.cs
@@ -7,6 +7,8 @@ using Horizon.RT.Models;
 using Horizon.LIBRARY.Pipeline.Udp;
 using System.Collections.Concurrent;
 using System.Net;
+using DotNetty.Buffers;
+using NetworkLibrary.Extension;
 
 namespace Horizon.BWPS
 {
@@ -18,13 +20,6 @@ namespace Horizon.BWPS
         protected IChannel? _boundChannel;
         protected SimpleDatagramHandler? _scertHandler;
 
-        //protected ClientObject ClientObject { get; set; } = null;
-
-        private ConcurrentQueue<ScertDatagramPacket> _recvQueue = new();
-        private ConcurrentQueue<ScertDatagramPacket> _sendQueue = new();
-
-        private BaseScertMessage? _lastMessage { get; set; } = null;
-
         /// <summary>
         /// Start the BWPS UDP Server.
         /// </summary>
@@ -35,76 +30,58 @@ namespace Horizon.BWPS
             _scertHandler = new SimpleDatagramHandler();
 
             // Queue all incoming messages
-            _scertHandler.OnChannelMessage += async (channel, message) =>
+            _scertHandler.OnChannelMessage += (channel, message) =>
             {
-                /*
-                var pluginArgs = new OnUdpMsg()
+                if (message.Sender is IPEndPoint sender && sender.Port != 0)
                 {
-                    //Player = ClientObject,
-                    Packet = message
-                };
-                */
-                // Plugin
-                //await Program.Plugins.OnEvent(PluginEvent.DME_GAME_ON_RECV_UDP, pluginArgs);
-
-                //if (!pluginArgs.Ignore)
-                //_recvQueue.Enqueue(message);
-
-                LoggerAccessor.LogInfo($"[BWPS] - Received Message: {message} on {channel}");
-
-                // Send ip and port back if the last byte isn't 0xD4
-
-                if (message.Content.ReadableBytes == 18)
-                {
-                    var buffer = channel.Allocator.Buffer(18);
-
-                    byte MessageId = message.Content.GetByte(0);
-                    var data = Array.Empty<byte>();
-
-                    if (message.Content.GetByte(9) == 0x05)
-                        data = new byte[] { MessageId, 0xc8, 0x01, 0x00, 0xcf, 0x5e, 0x0c, 0x50, 0x01, 0x00, 0x00, 0x00, 0x03, 0x03, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x3 };
-                    else
-                        data = new byte[] { MessageId, 0xc8, 0x01, 0x00, 0xcf, 0x5e, 0x0c, 0x50, 0x01, 0x00, 0x00, 0x00, 0x05, 0x03, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x3 };
-
-                    //var data = new byte[] { MessageId, 0x01, 0x00, 0x00, 0x00, 0x03, 0x03, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x3 };
-
-                    LoggerAccessor.LogInfo($"[BWPS] - Sending 18 len response");
-
-                    //buffer.WriteUnsignedShort((ushort)(message.Sender as IPEndPoint).Port);
-
-                    await channel.WriteAndFlushAsync(new DatagramPacket(buffer, message.Sender));
-                }
-                else if (message.Content.ReadableBytes == 18 && message.Content.GetByte(message.Content.ReaderIndex + 2) != 0x01)
-                {
-                    if (message.Content.ReadableBytes == 18)
+                    IByteBuffer directBuf = message.Content;
+                    if (directBuf.HasArray)
                     {
-                        var buffer = channel.Allocator.Buffer(18);
+                        byte[] MsgArray = new byte[directBuf.ReadableBytes];
+                        directBuf.GetBytes(directBuf.ReaderIndex, MsgArray);
 
-                        LoggerAccessor.LogInfo($"[BWPS] - Sending 18 response");
+                        if (MsgArray.Length == 18)
+                        {
+                            // log to console
+                            LoggerAccessor.LogInfo($"[BWPS] - Received External IP {sender.Address} & Port {(ushort)sender.Port} identification request, sending server identification as a response!");
 
-                        buffer.WriteBytes(message.Content);
-                        //buffer.WriteUnsignedShort((ushort)(message.Sender as IPEndPoint).Port);
+                            IByteBuffer buffer = channel.Allocator.Buffer(18);
 
-                        await channel.WriteAndFlushAsync(new DatagramPacket(buffer, message.Sender));
+                            buffer.WriteByte(MsgArray[0]); // MessageId
+                            buffer.WriteByte(MsgArray[1]);
+                            buffer.WriteByte(MsgArray[2]);
+                            buffer.WriteByte(MsgArray[3]);
+                            buffer.WriteIntLE(50982); // SequenceId
+                            buffer.WriteBytes("03 03 02 00 00 00 00 00 02 03".HexStringToByteArray());
+
+                            // send response message 3 times.
+                            DatagramPacket packet = new(buffer, sender);
+                            for (byte i = 0; i < 3; i++)
+                            {
+                                channel.WriteAndFlushAsync(packet);
+                            }
+                        }
+                        else if (MsgArray.Length == 6)
+                        {
+                            // log to console
+                            LoggerAccessor.LogInfo($"[BWPS] - Received External IP {sender.Address} & Port {(ushort)sender.Port} test request, sending server identification as a response!");
+
+                            IByteBuffer buffer = channel.Allocator.Buffer(6);
+
+                            buffer.WriteByte(MsgArray[0]); // MessageId
+                            buffer.WriteByte(MsgArray[1]);
+                            buffer.WriteByte(MsgArray[2]);
+                            buffer.WriteByte(MsgArray[3]);
+                            buffer.WriteIntLE(58595); // SequenceId
+
+                            // send response message 3 times.
+                            DatagramPacket packet = new(buffer, sender);
+                            for (byte i = 0; i < 3; i++)
+                            {
+                                channel.WriteAndFlushAsync(packet);
+                            }
+                        }
                     }
-                }
-
-                if (message.Content.ReadableBytes == 6)
-                {
-                    var buffer = channel.Allocator.Buffer(6);
-
-                    byte MessageId = message.Content.GetByte(0);
-                    byte Unk1 = message.Content.GetByte(1);
-
-                    byte[] padding = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-                    byte[] data = new byte[] { MessageId, Unk1, 0x01, 0x00, 0x01, 0x00 };
-
-                    LoggerAccessor.LogInfo($"[BWPS] - Sending 6 len response ");
-
-                    buffer.WriteBytes(data);
-                    //buffer.WriteUnsignedShort((ushort)(message.Sender as IPEndPoint).Port);
-
-                    await channel.WriteAndFlushAsync(new DatagramPacket(buffer, message.Sender));
                 }
             };
 
@@ -143,189 +120,5 @@ namespace Horizon.BWPS
                         _workerGroup.ShutdownGracefullyAsync(TimeSpan.FromMilliseconds(100), TimeSpan.FromSeconds(1)));
             }
         }
-
-        public Task Tick()
-        {
-            return Task.CompletedTask;
-        }
-
-        #region Message Processing
-
-        protected void ProcessMessage(ScertDatagramPacket packet)
-        {
-            var message = packet.Message;
-
-            switch (message)
-            {
-                /*
-                case RT_MSG_CLIENT_CONNECT_AUX_UDP connectAuxUdp:
-                    {
-                        var clientObject = Program.BWPS.GetClientByScertId(connectAuxUdp.ScertId);
-                        if (clientObject != ClientObject && ClientObject.DmeId != connectAuxUdp.PlayerId)
-                            break;
-
-                        //
-                        AuthenticatedEndPoint = packet.Source;
-
-                        ClientObject.RemoteUdpEndpoint = AuthenticatedEndPoint as IPEndPoint;
-                        ClientObject.OnUdpConnected();
-
-                        //
-                        var msg = new RT_MSG_SERVER_CONNECT_ACCEPT_AUX_UDP()
-                        {
-                            PlayerId = (ushort)ClientObject.DmeId,
-                            ScertId = ClientObject.ScertId,
-                            PlayerCount = (ushort)ClientObject.DmeWorld.Clients.Count,
-                            EndPoint = ClientObject.RemoteUdpEndpoint
-                        };
-
-                        // Send it twice in case of packet loss
-                        //_boundChannel.WriteAndFlushAsync(new ScertDatagramPacket(msg, packet.Source));
-                        _boundChannel.WriteAndFlushAsync(new ScertDatagramPacket(msg, packet.Source));
-                        break;
-                    }
-                case RT_MSG_SERVER_ECHO serverEchoReply:
-                    {
-
-                        break;
-                    }
-                case RT_MSG_CLIENT_ECHO clientEcho:
-                    {
-                        SendTo(new RT_MSG_CLIENT_ECHO() { Value = clientEcho.Value }, packet.Source);
-                        break;
-                    }
-                case RT_MSG_CLIENT_APP_BROADCAST clientAppBroadcast:
-                    {
-                        if (AuthenticatedEndPoint == null || !AuthenticatedEndPoint.Equals(packet.Source))
-                            break;
-
-                        ClientObject.DmeWorld?.BroadcastUdp(ClientObject, clientAppBroadcast.Payload);
-                        break;
-                    }
-                case RT_MSG_CLIENT_APP_LIST clientAppList:
-                    {
-                        if (AuthenticatedEndPoint == null || !AuthenticatedEndPoint.Equals(packet.Source))
-                            break;
-
-                        ClientObject.DmeWorld?.SendUdpAppList(ClientObject, clientAppList.Targets, clientAppList.Payload);
-                        break;
-                    }
-                case RT_MSG_CLIENT_APP_SINGLE clientAppSingle:
-                    {
-                        if (AuthenticatedEndPoint == null || !AuthenticatedEndPoint.Equals(packet.Source))
-                            break;
-
-                        ClientObject.DmeWorld?.SendUdpAppSingle(ClientObject, clientAppSingle.TargetOrSource, clientAppSingle.Payload);
-                        break;
-                    }
-                case RT_MSG_CLIENT_APP_TOSERVER clientAppToServer:
-                    {
-                        if (AuthenticatedEndPoint == null || !AuthenticatedEndPoint.Equals(packet.Source))
-                            break;
-
-                        ProcessMediusMessage(clientAppToServer.Message);
-                        break;
-                    }
-
-                case RT_MSG_CLIENT_DISCONNECT _:
-                case RT_MSG_CLIENT_DISCONNECT_WITH_REASON _:
-                    {
-
-                        break;
-                    }
-                */
-                default:
-                    {
-                        LoggerAccessor.LogInfo($"[BWPS] - UNHANDLED RT MESSAGE: {message}");
-
-                        break;
-                    }
-            }
-
-            return;
-        }
-
-        protected virtual void ProcessMediusMessage(BaseMediusMessage message)
-        {
-            if (message == null)
-                return;
-        }
-
-        #endregion
-
-        #region Send
-
-        private void SendTo(BaseScertMessage message, EndPoint target)
-        {
-            if (target == null)
-                return;
-
-            _sendQueue.Enqueue(new ScertDatagramPacket(message, target));
-        }
-
-
-        #endregion
-
-        #region Tick
-
-        public Task HandleIncomingMessages()
-        {
-            if (_boundChannel == null || !BWPSClass.started)
-                return Task.CompletedTask;
-
-            try
-            {
-                // Process all messages in queue
-                while (_recvQueue.TryDequeue(out var message))
-                {
-                    try
-                    {
-                        //if (!await PassMessageToPlugins(_boundChannel, ClientObject, message.Message, true))
-                        ProcessMessage(message);
-                    }
-                    catch (Exception e)
-                    {
-                        LoggerAccessor.LogError(e);
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                LoggerAccessor.LogError(e);
-            }
-
-            return Task.CompletedTask;
-        }
-
-        public async Task HandleOutgoingMessages()
-        {
-            if (_boundChannel == null || !BWPSClass.started)
-                return;
-
-            List<ScertDatagramPacket> responses = new List<ScertDatagramPacket>();
-
-            try
-            {
-                // Send if writeable
-                if (_boundChannel.IsWritable)
-                {
-                    // Add send queue to responses
-                    while (_sendQueue.TryDequeue(out var message))
-                    {
-                        //if (!await PassMessageToPlugins(_boundChannel, ClientObject, message.Message, false))
-                        responses.Add(message);
-                    }
-
-                    if (responses.Count > 0)
-                        await _boundChannel.WriteAndFlushAsync(responses);
-                }
-            }
-            catch (Exception e)
-            {
-                LoggerAccessor.LogError(e);
-            }
-        }
-
-        #endregion
     }
 }
