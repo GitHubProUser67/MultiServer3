0f7de53347a1231b7016ed78824c32b556e2e9d3
GitHubProUser67 <127040195+GitHubProUser67@users.noreply.github.com>
2025-02-22 17:47:40 +0100

    [WebAPIService/HTTP/HTTPS] - Implements first take on HeavyWater PSHome APIs.
    This commit implements the Avalon Keep API endpoints, this also sets the floor for HeavyWater API methods.
    
    Also fixup a small oversight in the docker launch settings.

---
+++ AuxiliaryServices/WebAPIService/HEAVYWATER/HeavyWaterClass.cs
@@ -0,0 +1,288 @@
+﻿using CustomLogger;
+using HttpMultipartParser;
+using NetworkLibrary.Extension;
+using NetworkLibrary.HTTP;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+namespace WebAPIService.HEAVYWATER
+{
+    public class HeavyWaterClass
+    {
+        private string absolutepath;
+        private string method;
+        private string apipath;
+
+        public HeavyWaterClass(string method, string absolutepath, string apipath)
+        {
+            this.absolutepath = absolutepath;
+            this.method = method;
+            this.apipath = apipath;
+        }
+
+        public string ProcessRequest(byte[] PostData, string ContentType)
+        {
+            const string playerPathern = @"/player/([^/]+)";
+
+            try
+            {
+                Match match = Regex.Match(absolutepath, playerPathern);
+
+                switch (method)
+                {
+                    case "GET":
+                        if (match.Success)
+                        {
+                            string id = match.Groups[1].Value;
+
+                            if (absolutepath.Contains("/D2O/Avalon/"))
+                            {
+                                string avalonProfilePath = apipath + $"/HEAVYWATER/Avalon_keep/{id}/";
+
+                                if (absolutepath.EndsWith("/data/HouseData"))
+                                {
+                                    string house = "{\"House\":{\"AVALON\":true}}";
+
+                                    if (File.Exists(avalonProfilePath + "/House.json"))
+                                        house = File.ReadAllText(avalonProfilePath + "/House.json");
+
+                                    return $@"{{
+                                    ""STATUS"": ""SUCCESS"",
+                                    ""result"":
+                                      {house}
+                                  }}";
+                                }
+                                else if (absolutepath.EndsWith("/data/MyAvalonKeepData"))
+                                {
+                                    string AvalonKeepData = "{}";
+
+                                    if (File.Exists(avalonProfilePath + "/AvalonKeepData.json"))
+                                        AvalonKeepData = File.ReadAllText(avalonProfilePath + "/AvalonKeepData.json");
+
+                                    return $@"{{
+                                    ""STATUS"": ""SUCCESS"",
+                                    ""result"":
+                                      {AvalonKeepData}
+                                  }}";
+                                }
+                            }
+                            else if (absolutepath.Contains("/D2O/D2OUniverse/"))
+                            {
+                                string ProfilePath = apipath + $"/HEAVYWATER/D2OUniverse/{id}";
+
+                                if (absolutepath.EndsWith("/data/D2OData"))
+                                {
+                                    string D2OData = "{}";
+
+                                    if (File.Exists(ProfilePath + "/D2OData.json"))
+                                        D2OData = File.ReadAllText(ProfilePath + "/D2OData.json");
+
+                                    return $@"{{
+                                    ""STATUS"": ""SUCCESS"",
+                                    ""result"":
+                                      {D2OData}
+                                  }}";
+                                }
+                            }
+                        }
+                        else if (absolutepath.Contains("/D2O/Avalon/"))
+                        {
+                            if (absolutepath.EndsWith("/contributions"))
+                            {
+                                string ContribData = "{\"Contribution\":{}}";
+                                string ContribPath = apipath + $"/HEAVYWATER/Avalon_keep/contributions.json";
+
+                                if (File.Exists(ContribPath))
+                                    ContribData = File.ReadAllText(ContribPath);
+
+                                return $@"{{
+                                    ""STATUS"": ""SUCCESS"",
+                                    ""result"":
+                                      {ContribData}
+                                  }}";
+                            }
+                        }
+                        break;
+                    case "POST":
+                        if (absolutepath.Contains("/D2O/Ticket/verify/"))
+                        {
+                            using (MemoryStream copyStream = new MemoryStream(PostData))
+                            {
+                                foreach (FilePart file in MultipartFormDataParser.Parse(copyStream, HTTPProcessor.ExtractBoundary(ContentType))
+                                    .Files.Where(x => x.FileName == "ticket.bin" && x.Name == "base64-ticket"))
+                                {
+                                    using (Stream filedata = file.Data)
+                                    {
+                                        filedata.Position = 0;
+
+                                        // Find the number of bytes in the stream
+                                        int contentLength = (int)filedata.Length;
+
+                                        // Create a byte array
+                                        byte[] ticketData = new byte[contentLength];
+
+                                        // Read the contents of the memory stream into the byte array
+                                        filedata.Read(ticketData, 0, contentLength);
+
+                                        (bool, byte[]) isValidBase64Data = new string(Encoding.ASCII.GetString(ticketData).Where(c => !char.IsWhiteSpace(c)).ToArray()).IsBase64();
+
+                                        if (isValidBase64Data.Item1)
+                                        {
+                                            // Extract the desired portion of the binary data for a npticket 4.0
+                                            byte[] extractedData = new byte[0x63 - 0x54 + 1];
+
+                                            // Copy it
+                                            Array.Copy(isValidBase64Data.Item2, 0x54, extractedData, 0, extractedData.Length);
+
+                                            // Trim null bytes
+                                            int nullByteIndex = Array.IndexOf(extractedData, (byte)0x00);
+                                            if (nullByteIndex >= 0)
+                                            {
+                                                byte[] trimmedData = new byte[nullByteIndex];
+                                                Array.Copy(extractedData, trimmedData, nullByteIndex);
+                                                extractedData = trimmedData;
+                                            }
+
+                                            string UserId = Encoding.UTF8.GetString(extractedData);
+
+                                            if (ByteUtils.FindBytePattern(isValidBase64Data.Item2, new byte[] { 0x52, 0x50, 0x43, 0x4E }, 184) != -1)
+                                                LoggerAccessor.LogInfo($"[HeavyWaterClass] - ProcessRequest : User {UserId} logged in and is on RPCN");
+                                            else
+                                                LoggerAccessor.LogInfo($"[HeavyWaterClass] - ProcessRequest : User {UserId} logged in and is on PSN");
+
+                                            string id = GenerateD2OGuid(UserId);
+
+                                            return $@"{{
+                                                    ""STATUS"": ""SUCCESS"",
+                                                    ""result"": {{
+                                                      ""d2oID"": ""{id}""
+                                                    }}
+                                                  }}";
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        break;
+                    case "PUT":
+                        if (PostData.Length > 0 && PostData.Length <= Array.MaxLength)
+                        {
+                            if (match.Success)
+                            {
+                                string id = match.Groups[1].Value;
+
+                                if (absolutepath.Contains("/D2O/Avalon/"))
+                                {
+                                    string avalonProfilePath = apipath + $"/HEAVYWATER/Avalon_keep/{id}/";
+
+                                    Directory.CreateDirectory(avalonProfilePath);
+
+                                    if (absolutepath.EndsWith("/data/HouseData"))
+                                    {
+                                        string house = Encoding.UTF8.GetString(PostData);
+                                        File.WriteAllText(avalonProfilePath + "/House.json", house);
+
+                                        return $@"{{
+                                            ""STATUS"": ""SUCCESS"",
+                                            ""result"":
+                                              {house}
+                                          }}";
+                                    }
+                                    else if (absolutepath.EndsWith("/data/MyAvalonKeepData"))
+                                    {
+                                        string AvalonKeepData = Encoding.UTF8.GetString(PostData);
+                                        File.WriteAllText(avalonProfilePath + "/AvalonKeepData.json", AvalonKeepData);
+
+                                        return $@"{{
+                                            ""STATUS"": ""SUCCESS"",
+                                            ""result"":
+                                              {AvalonKeepData}
+                                          }}";
+                                    }
+                                }
+                                else if (absolutepath.Contains("/D2O/D2OUniverse/"))
+                                {
+                                    string ProfilePath = apipath + $"/HEAVYWATER/D2OUniverse/{id}";
+
+                                    Directory.CreateDirectory(ProfilePath);
+
+                                    if (absolutepath.EndsWith("/data/D2OData"))
+                                    {
+                                        string D2OData = Encoding.UTF8.GetString(PostData);
+                                        File.WriteAllText(ProfilePath + "/D2OData.json", D2OData);
+
+                                        return $@"{{
+                                            ""STATUS"": ""SUCCESS"",
+                                            ""result"": 
+                                              {D2OData}
+                                          }}";
+                                    }
+                                }
+                            }
+                            else if (absolutepath.Contains("/D2O/Avalon/"))
+                            {
+                                if (absolutepath.EndsWith("/metrics"))
+                                {
+                                    // TODO: process metrics data?
+                                    return @"{
+                                        ""STATUS"": ""SUCCESS"",
+                                      }";
+                                }
+                                else if (absolutepath.EndsWith("/contributions"))
+                                {
+                                    Directory.CreateDirectory(apipath + $"/HEAVYWATER/Avalon_keep");
+
+                                    string ContribPath = apipath + $"/HEAVYWATER/Avalon_keep/contributions.json";
+
+                                    using (JsonDocument requestDoc = JsonDocument.Parse(Encoding.UTF8.GetString(PostData)))
+                                    {
+                                        JsonElement contribution = requestDoc.RootElement.GetProperty("Contribution");
+                                        string house = contribution.GetProperty("House").GetString();
+                                        int amount = contribution.GetProperty("Amount").GetInt32();
+
+                                        if (!File.Exists(ContribPath))
+                                            File.WriteAllText(ContribPath, $"{{\"Contribution\":{{\"{house}\":{amount}}}");
+                                        else
+                                        {
+                                            JObject existingData = JsonConvert.DeserializeObject<JObject>(File.ReadAllText(ContribPath));
+
+                                            JObject publicContribution = existingData["Contribution"] as JObject;
+                                            if (publicContribution != null)
+                                            {
+                                                if (publicContribution[house] != null)
+                                                    amount += publicContribution[house].ToObject<int>();
+
+                                                publicContribution[house] = amount;
+
+                                                File.WriteAllText(ContribPath, existingData.ToString());
+                                            }
+                                        }
+
+                                        return "{\"STATUS\": \"SUCCESS\"}";
+                                    }
+                                }
+                            }
+                        }
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                LoggerAccessor.LogError($"[HeavyWaterClass] - ProcessRequest thrown an assertion. (Exception: {ex})");
+            }
+
+            return null;
+        }
+
+        private static string GenerateD2OGuid(string input)
+        {
+            return SSFW.GuidGenerator.SSFWGenerateGuid(input, "1amAH3vyFan?!0yY3ahhhhhhhh!!!!!");
+        }
+    }
+}
